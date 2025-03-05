0ee4afd5dbf7f2f2930d58f20d26d7113bc36b2c
MJ Munar <mjmunar.dev@gmail.com>
2025-03-05 21:07:33 +0800

    feat: better caching

--- lib/controller/data_store_controller.dart
+++ lib/controller/data_store_controller.dart
@@ -1,28 +1,44 @@
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DataStoreController extends GetxController {
   // Create an observable Map to hold dynamic data
   RxString sugarFundsBalance = "".obs;
+  final data = RxMap<String, dynamic>();
+  late SharedPreferences prefs;
 
-  var data = <String, dynamic>{}.obs;
+  @override
+  void onInit() async {
+    super.onInit();
+    prefs = await SharedPreferences.getInstance();
+    // Load any saved boolean values
+    final keys = prefs.getKeys();
+    for (final key in keys) {
+      data[key] = prefs.get(key);
+    }
+  }
 
   // Method to add or update data dynamically
   void setData(String key, dynamic value) {
-    data[key] = value; // Insert or update the key-value pair
+    data[key] = value; // Update in-memory
+    _saveToPrefs(key, value); // Persist to SharedPreferences
   }
 
-  // Method to retrieve data by key
+  // Method to get data
   dynamic getData(String key) {
-    return data[key]; // Return the value associated with the key
+    return data[key];
   }
 
-  // Method to remove a key-value pair from the Map
-  void removeData(String key) {
-    data.remove(key);
-  }
-
-  // Method to check if a key exists
-  bool containsKey(String key) {
-    return data.containsKey(key);
+  // Private method to save to SharedPreferences
+  void _saveToPrefs(String key, dynamic value) async {
+    if (value is bool) {
+      await prefs.setBool(key, value);
+    } else if (value is String) {
+      await prefs.setString(key, value);
+    } else if (value is int) {
+      await prefs.setInt(key, value);
+    } else if (value is double) {
+      await prefs.setDouble(key, value);
+    }
   }
 }
